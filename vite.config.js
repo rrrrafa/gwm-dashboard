699d1b7dfae55fd56817073a66752c05d030127f
rrrrafa <gallerywallmockups@gmail.com>
2026-03-10 22:28:09 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-export default defineConfig({ plugins: [react()] })
+
+export default defineConfig({
+  plugins: [react()],
+  esbuild: {
+    include: /src\/.*\.[jt]sx?$/,
+    loader: 'jsx',
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: { '.js': 'jsx' }
+    }
+  }
+})
